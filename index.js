9d0f7b841d9786fbee2394398b59df6e15643b2a
nobelium24 <ogunbaja07@gmail.com>
2023-05-17 13:52:31 +0100

    CRUD class part 1

--- index.js
+++ index.js
@@ -1,50 +1,83 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (request, response)=>{
+app.get("/", (request, response) => {
     response.send([
-        {name:"Adewole", age:1111, food:"Beans and bread"},
-        {name:"Uthman", age:1120, food:"Rice and beans"},
-        {name:"Samuel", age:7, food:"Iyan and Egusi"},
-        {name:"Aishat", age:133, food:"Fired rice and chicken"},
-        {name:"Issac", age:25, food:"Beans and bread"},
-        {name:"Comfort", age:14, food:"Ewa agoyin and yam"},
-        {name:"Olu", age:40, food:"fufu and egusi"},
+        { name: "Adewole", age: 1111, food: "Beans and bread" },
+        { name: "Uthman", age: 1120, food: "Rice and beans" },
+        { name: "Samuel", age: 7, food: "Iyan and Egusi" },
+        { name: "Aishat", age: 133, food: "Fired rice and chicken" },
+        { name: "Issac", age: 25, food: "Beans and bread" },
+        { name: "Comfort", age: 14, food: "Ewa agoyin and yam" },
+        { name: "Olu", age: 40, food: "fufu and egusi" },
     ])
 })
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    text: String
+})
+const todoModel = mongoose.models.todo_tbs || mongoose.model("todo_tbs", todoSchema)
 
 const todoArray = []
 let id
 
-app.get("/html", (req, res)=>{
+app.get("/html", (req, res) => {
     console.log(__dirname)
     res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/todo", (req, res)=>{
-    res.render("index", {todoArray: todoArray});
+app.get("/todo", async (req, res) => {
+    try {
+        const result = await todoModel.find({})
+        if(!result){
+            return res.status(500).send({ message: "Error fetching from database", status: false })
+        }
+        console.log(result)
+        res.render("index", { todoArray: result });
+    } catch (error) {
+        return res.status(500).send({message:"Internal server error", status:false})
+    }
 })
 
-app.post("/todo", (req, res)=>{
+app.post("/todo", async (req, res) => {
     console.log(req.body)
-    todoArray.push(req.body)
-    console.log(todoArray)
-    res.render("index", {todoArray: todoArray})
+    const { title, text } = req.body
+    try {
+        // const todo = new todoModel({
+        //     title,
+        //     text
+        // })
+        // const result = await todo.save()
+        //or
+        const result = await todoModel.create({ title, text })
+        if (!result) {
+            return res.status(500).send({ message: "Error saving to database", status: false })
+        }
+        // console.log(result)
+        res.redirect("/todo")
+        // return res.status(201).send({ message: "Todo created successfully", status: true })
+    } catch (error) {
+        return res.status(500).send({message:"Internal server error", status:false})
+    }
+
 })
 
-app.post("/delete", (req, res)=>{
+app.post("/delete", (req, res) => {
     let index = req.body.index
     console.log(index)
     todoArray.splice(index, 1)
     res.redirect("/todo")
 })
 
-app.get("/edit/:id", (req, res)=>{
+app.get("/edit/:id", (req, res) => {
     // console.log(req.params.id)
     id = req.params.id
     let title
@@ -54,16 +87,27 @@ app.get("/edit/:id", (req, res)=>{
         title = todoArray[id].title;
         text = todoArray[id].text
     }
-    res.render("edit", {todoArray:todoArray, title, text})
+    res.render("edit", { todoArray: todoArray, title, text })
 })
 
-app.post("/edittodo", (req, res)=>{
+app.post("/edittodo", (req, res) => {
     console.log(req.body, id)
     let obj = req.body
     todoArray.splice(id, 1, obj)
     res.redirect("/todo")
 })
 
-const server = app.listen("3333", ()=>{
-    console.log("The serer has started on port 3333"); 
+const uri = process.env.MONGODB_URI
+const connect = () => {
+    try {
+        mongoose.set("strictQuery", false)
+        mongoose.connect(uri)
+        console.log("MongoDB connected")
+    } catch (error) {
+        console.log(error)
+    }
+}
+connect()
+const server = app.listen("3333", () => {
+    console.log("The serer has started on port 3333");
 })
